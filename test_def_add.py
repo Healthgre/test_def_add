d5d8a3b5a0124d1360e4fde27d0b6a9a21106404
Healthgre <69686199+Healthgre@users.noreply.github.com>
2024-03-24 17:57:30 +0100

    Initial commit

---
+++ test_def_add.py
@@ -0,0 +1,46 @@
+import pytest
+
+
+# Пример функции, которую мы будем тестировать
+def add(a, b):
+    return a + b
+
+
+# Тестирование функции с использованием PyTest
+def test_add_positive_positive():
+    assert add(3, 5) == 8
+
+
+def test_add_negative_positive():
+    assert add(-3, 5) == 2
+
+
+def test_add_negative_negative():
+    assert add(-3, -5) == -8
+
+
+def test_add_positive_zero():
+    assert add(5, 0) == 5
+
+
+def test_add_negative_zero():
+    assert add(-5, 0) == -5
+
+
+def test_add_zero_zero():
+    assert add(0, 0) == 0
+
+
+def test_add_int_string():
+    with pytest.raises(TypeError):
+        add(5, "string")
+
+
+def test_addition_with_one_argument():
+    with pytest.raises(TypeError):
+        add(5)
+
+
+def test_addition_with_3_argument():
+    with pytest.raises(TypeError):
+        add(5, 6, 7)
